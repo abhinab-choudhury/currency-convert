ce6ab0f0f3110c951a7334f80eae37792b92b875
abhinab-choudhury <abhinabchoudhury291@gmail.com>
2023-10-04 18:39:36 +0530

    First-Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Box from './components/box/box'
+function App() {
+
+  return (
+    <>
+      <Box/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/box/box.jsx
@@ -0,0 +1,13 @@
+import './box.css'
+
+export default function box() {
+  return (
+    <div>
+      <div className="center-box">
+        <div className="box">
+            
+        </div>
+      </div>
+    </div>
+  )
+}
